d424caa480f79071dd868877d2afc9281ae23373
Hugo <hugohsilva_@hotmail.com>
2023-05-11 09:51:56 -0300

    :bug: gambiarra para funcionar click e keypress

--- index.js
+++ index.js
@@ -4,9 +4,76 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
+search.addEventListener('click', () => {
+    const APIKey = '080854301d100b0230fdc63491cbd819';
+        const city = document.querySelector(".search-box input").value;
+
+        if (city === '')
+            return;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+
+            if(json.cod === '404'){
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.display = 'none';
+                error404.style.display ='block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main){
+                case 'Clear':
+                    image.src = "image/clear.png";
+                    break;
+                
+                case 'Rain':
+                    image.src = "image/rain.png";
+                    break;
+
+                case 'Snow':
+                    image.src = "image/snow.png";
+                    break;   
+
+                case 'Clouds':
+                    image.src = "image/cloud.png";
+                    break;  
+
+                case 'Mist':
+                    image.src = "image/mist.png";
+                    break;  
+                
+                default:
+                    image.src = '';
+
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+        });
+});
+
+
 search.addEventListener('keypress', (e) => {
 
-    if(e.key === 'Enter') {
+    if(e.key === 'Enter' || e === 'Click') {
     
         const APIKey = '080854301d100b0230fdc63491cbd819';
         const city = document.querySelector(".search-box input").value;
@@ -71,13 +138,6 @@ search.addEventListener('keypress', (e) => {
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
 
-
-
-
         });
-
     };
-
-    
-
 });
