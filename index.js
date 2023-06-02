85c7487b228e6e2d40614f246c8403db63a33422
Hugo <hugohsilva_@hotmail.com>
2023-06-02 19:34:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const error404 = document.querySelector('.not-found');
 
 /* `search.addEventListener('click', () => {` is adding an event listener to the search button. When
 the button is clicked, the function inside the arrow function will be executed. */
-search.addEventListener('click', () => {
+search.addEventListener('click', searchWeather);
+search.addEventListener('keypress', searchWeather);
+function searchWeather() {
     const APIKey = '080854301d100b0230fdc63491cbd819';
         const city = document.querySelector(".search-box input").value;
 
@@ -82,82 +84,4 @@ search.addEventListener('click', () => {
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
         });
-});
-
-
-/* `search.addEventListener('keypress', (e) =>` is adding an event listener to the search input field.
-When a key is pressed while the input field is in focus, the function inside the arrow function will
-be executed. In this case, the function checks if the key pressed is the "Enter" key, and if it is,
-it triggers the same functionality as the click event listener added to the search button. This
-allows the user to search for a city by either clicking the search button or pressing the "Enter"
-key while the search input field is in focus. */
-search.addEventListener('keypress', (e) => {
-
-    if(e.key === 'Enter') {
-    
-        const APIKey = '080854301d100b0230fdc63491cbd819';
-        const city = document.querySelector(".search-box input").value;
-
-        if (city === '')
-            return;
-
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
-
-            if(json.cod === '404'){
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.display = 'none';
-                error404.style.display ='block';
-                error404.classList.add('fadeIn');
-                return;
-            }
-
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
-
-            const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('.weather-details .wind span');
-
-            switch (json.weather[0].main){
-                case 'Clear':
-                    image.src = "image/clear.png";
-                    break;
-                
-                case 'Rain':
-                    image.src = "image/rain.png";
-                    break;
-
-                case 'Snow':
-                    image.src = "image/snow.png";
-                    break;   
-
-                case 'Clouds':
-                    image.src = "image/cloud.png";
-                    break;  
-
-                case 'Mist':
-                    image.src = "image/mist.png";
-                    break;  
-                
-                default:
-                    image.src = '';
-
-            }
-
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classList.add('fadeIn');
-            weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
-
-        });
-    };
-});
+};
